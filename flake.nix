be9aa9b3db2cf65a1e11e9b7f2630d769cefab86
ForbiddenLiquid <jroneill2004@gmail.com>
2026-03-18 23:01:05 -0400

    Main config and flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Baby's first flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+	./configuration.nix
+      ];
+    };
+
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = [
+	pkgs.python3
+      ];
+    };
+
+  };
+}
